427de2f0ba047b8f4769e1f3212717d2ab5213ba
Rondywastaken <itzrondy@gmail.com>
2023-04-11 13:18:40 +0200

    updating dashboard

--- app/src/routes/dashboard/+page.svelte
+++ app/src/routes/dashboard/+page.svelte
@@ -1,13 +1,19 @@
 
 <script lang="ts">
+    import "./dashboard.css";
+
     export let data;
 </script>
 
 <div class="window-container">
     <div class="app-container">
-        <h2>Velkommen {data.user.username}</h2>
-        <form action="?/logout" method="POST">
-            <button>Log ud</button>
-        </form>
+
+        <div class="dashboard-container">
+            <h2>Velkommen {data.user.username}</h2>
+            <form action="?/logout" method="POST">
+                <button>Log ud</button>
+            </form>
+        </div>
+        
     </div>
 </div>  
